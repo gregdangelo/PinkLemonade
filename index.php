7ce9fbd45b31dbf8b799093d2ec0db6f136c951e
Greg D'Angelo <gregdangelo@gmail.com>
2012-11-20 22:09:42 -0500

    simple tests

--- index.php
+++ index.php
@@ -6,11 +6,14 @@ include('PinkLemonade.php');
 	<title>PinkLemonade</title>
 	<link href="#" rel="stylesheet" type="text/css"/>
 </head>
-<body>
+<body style="background-color:#ccc;">
+<a href="#Red" onclick="makeRed();return false;">Red</a>
 <?
 	$pk = new PinkLemonade;
 	$pk->css_filename('test.css');
-	$pk->sprite('sp_test.png',__DIR__.'/images');
+	//$pk->sprite('sp_test.png',__DIR__.'/images');
+	$pk->sprite('sp_test.png',__DIR__.'/images/twn');
+	
 	echo __DIR__;
 	//$pk->viewTrees();
 	$pk->save();
@@ -37,5 +40,41 @@ include('PinkLemonade.php');
 <img src="sprites/sp_test.png"/>
 <img src="images/logo.png"/>
 <img src="sprites/sp_alpha.png"/>
+<img src="images/twn/app-android.png"/>
+<script>
+function makeRed(){
+	var elm = document.getElementsByTagName('body');
+	elm = elm[0];
+	elm.style.backgroundColor = "#F00";
+	var elm = document.getElementsByTagName('a');
+	elm = elm[0];
+	elm.href = "#Grey";
+	elm.onclick = makeGrey;
+	elm.innerHTML = "Grey";
+	return false;
+}
+function makeGrey(){
+	var elm = document.getElementsByTagName('body');
+	elm = elm[0];
+	elm.style.backgroundColor = "#CCC";
+	var elm = document.getElementsByTagName('a');
+	elm = elm[0];
+	elm.href = "#White";
+	elm.onclick = makeWhite;
+	elm.innerHTML = "White";
+	return false;
+}
+function makeWhite(){
+	var elm = document.getElementsByTagName('body');
+	elm = elm[0];
+	elm.style.backgroundColor = "#FFF";
+	var elm = document.getElementsByTagName('a');
+	elm = elm[0];
+	elm.href = "#Red";
+	elm.onclick = makeRed;
+	elm.innerHTML = "Red";
+	return false;
+}
+</script>
 </body>
 </html>
